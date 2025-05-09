6344c30faf4816dd529c3946bc17f0db989f2075
Sahil Rana <sahilrana27582@gmail.com>
2025-05-09 20:40:09 +0530

    Folder Structure Ready

---
+++ user_service/src/test/java/com/logistic_management_system/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistic_management_system.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
